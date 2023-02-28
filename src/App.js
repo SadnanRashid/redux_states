7e1bc6f0aade1d0457cad76750fe5cc418c18113
SadnanRashid <sadnan.rashid.07@gmail.com>
2023-02-28 14:23:35 +0600

    ss

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
+import Child from "./child";
+import { createContext } from "react";
 
 function App() {
+  const CONTEXT = createContext();
   const [count, setCount] = useState(0);
+  const value = { count, setCount };
   function handleAdd() {
     setCount(count + 1);
   }
@@ -12,6 +16,9 @@ function App() {
       <h1>The counter app</h1>
       <h3>{count}</h3>
       <button onClick={handleAdd}>+</button>
+      <CONTEXT.Provider value={value}>
+        <Child />
+      </CONTEXT.Provider>
     </div>
   );
 }
---
+++ src/child.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Child() {
+  return (
+    <div>
+      <h1>This is child counter</h1>
+      <h2>count</h2>
+    </div>
+  );
+}
