7b4f287f23ba9ac7c9238769a4c0e0d64d78f9e2
SadnanRashid <sadnan.rashid.07@gmail.com>
2023-02-28 14:19:11 +0600

    s

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+  function handleAdd() {
+    setCount(count + 1);
+  }
   return (
     <div className="App">
       <h1>The counter app</h1>
+      <h3>{count}</h3>
+      <button onClick={handleAdd}>+</button>
     </div>
   );
 }
